3f6c5865c11b5df3fd3c8d1515fde15548b2725e
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-06-27 12:13:42 +0530

    Create 16x16 grid with borders

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    row.classList.value = 'row';
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        const box = document.createElement('div');
+        box.classList.value = 'box';
+        row.appendChild(box);
+    }
+}
