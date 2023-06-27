f8e5a2f5b6ea839ab8d96591bf0d786c8f5e4894
faisalsherif7 <116718026+faisalsherif7@users.noreply.github.com>
2023-06-27 21:01:39 +0530

    Complete project
    Moved hover from css to javascript. Boxes are now created dynamically
    when size is changed.

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const container = document.querySelector('.container');
+let gridSize = 16;
+createGrid(gridSize);
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.value = 'row';
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        const box = document.createElement('div');
-        box.classList.value = 'box';
-        row.appendChild(box);
+function createGrid(size) {
+    const boxSize = 600 / size;
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.value = 'row';
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const box = document.createElement('div');
+            box.classList.value = 'box';
+            box.style.width = `${boxSize}px`;
+            box.style.height = `${boxSize}px`;
+            row.appendChild(box);
+        }
     }
+    listenForHover();
+}
+
+const sizeButton = document.querySelector('.size-button')
+sizeButton.addEventListener('click', () => {
+    size = prompt('Enter number of squares per side: ');
+    if (!(size > 0) || !(size <= 100)) {
+        alert('Please enter a value between 1 and 100');
+        return;
+    }
+    container.innerHTML = '';
+    createGrid(size);
+})
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => box.style.backgroundColor = 'transparent');
+})
+
+function listenForHover() {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'teal';
+    }))
 }
